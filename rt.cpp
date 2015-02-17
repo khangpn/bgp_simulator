e2cbc343d1a564b8206a3a18d9af49d268afc0da
Jussi Heino <jussi.heino@gmail.com>
2015-02-17 12:19:56 +0200

    RT: route adding works, route count query works

--- rt.cpp
+++ rt.cpp
@@ -1,3 +1,16 @@
+/**
+ * @file   rt.cpp
+ * @Author Jussi Heino (juhehe@utu.fi
+ * @date   Feb 2015
+ * @brief  Route Table class impements a Internet routing table data structure and methods
+ *
+ * You can add routes to routing table.
+ * Assumes int is 32-bit (at least).
+ * Uses int to store a IPv4 address.
+ *
+ * \to-do { Currently a hard-coded limit of 1000 route entries}
+ */
+
 #include <iostream>
 #include <cstring>      // Needed for memset
 #include <sys/socket.h> // Needed for the socket functions
@@ -14,52 +27,44 @@ using namespace std;
 
 class routeTable
 {
+// currently hard-coded limit for 1000 routes, I will implement dynamic allocation using e.g. <vector>
 #define RMAX 1000
-	struct routingItem_t
-		{
-			unsigned long int nextHop; // where to next
-			unsigned long int target; // what is target IP address
-			unsigned long int mask; // mask generated with prefixlen
-			unsigned long int prefixlen; // IP/prefilen, the effectice bits in src
-			string ASPATH; // for informative purposes or advanced routing decisions
-		};
-	struct routingTable_t
-	{
-		routingItem_t ri[RMAX];
-		int items; // how many table entries in table
-	} rt;
+
+struct routingItem_t
+{
+	unsigned int nextHop; // where to next (IP address of the neighbor AS)
+	unsigned int target; // what is target IP address
+	unsigned int mask; // mask generated with prefixlen
+	unsigned int prefixlen; // IP/prefixlen, the effective bits in src
+	string ASPATH; // for informative purposes or advanced routing decisions
+	string ASID; // \to-do { should this be ASNAME as more descriptive convention }
+	int priority; // pri 0 for default route, all other routest pri at least 1
+
+};
+
+struct routingTable_t
+{
+	routingItem_t ri[RMAX];
+	int items; // how many table entries in table
+} rt;
 
 public:
 
-
-
 routeTable() {
-
-	routingItem_t rtRow;
 
 	// routing table, fixed side max 1000 entries now
 
-	// init rt:
+	// init rt item count:
 	rt.items = 0;
-
-	// method: add items to rt
-	string s = "50 701 42";
-	rtRow.ASPATH = s;
-	rtRow.mask = 24;
-	rtRow.nextHop = 10*8*8*8+ 10*8*8+  0*8+1;
-	rtRow.target = 130*8*8*8+232*8*8+232*8+0;
-	rt.ri[rt.items] = rtRow;
-	rt.items++;
-
 
 }; // Constructor
 
 ~routeTable(){};// Destructor
-//int addRoute();
-int addRoute(string ASPATH, unsigned long int mask, unsigned long int nextHop, unsigned long int target);
+
+int addRoute(string ASID, string ASPATH, unsigned int mask, unsigned int nextHop, unsigned int target, int priority);
 int queryRoute(int destination);
 int deleteRoute();
-int RouteCount();
+int routeCount();
 };
 
 /*int routeTable::routeTable()
@@ -67,27 +72,49 @@ int RouteCount();
 	//return 0;
 }*/
 
-int routeTable::addRoute(string ASPATH, unsigned long int mask, unsigned long int nextHop, unsigned long int target)
+int routeTable::addRoute(string ASID, string ASPATH,
+							unsigned int mask,
+							unsigned int nextHop,
+							unsigned int target,
+							int priority)
 {
 	routingItem_t rtRow;
+
 	rtRow.ASPATH = ASPATH;
-	rtRow.mask = 24;
-	rtRow.nextHop = 10*8*8*8+ 10*8*8+  0*8+1;
-	rtRow.target = 130*8*8*8+232*8*8+232*8+0;
+	rtRow.mask = mask;
+	rtRow.nextHop = nextHop;
+	rtRow.target = target;
+
 	rt.ri[rt.items] = rtRow;
 	rt.items++;
-
 
 	return 0;
 }
 
 int routeTable::queryRoute(int destination)
 {
-	return 1;
+	return rt.ri[0].nextHop;
 }
 
-int routeTable::RouteCount()
+/**
+ * @name    route count
+ * @brief   How many routes in routing table
+ * @ingroup routetable
+ *
+ * Finds the number of routes in routing table.
+ *
+ * @param [in] ()- No parameters
+ *
+ * @retval int Returns zero or larger integer as the number of routes in routing table.
+ *
+ * Example Usage:
+ * @code
+ * 		myRT routeTable;
+ * 		cout << myRT.routeCount(); // should output zero, since no routes were added
+ * @endcode
+ */
+int routeTable::routeCount()
 {
 	return rt.items;
-}
+} // ::routeCount
 
--- rt_testing.cpp
+++ rt_testing.cpp
@@ -1,15 +1,28 @@
 #include "rt.cpp"
 
-/*
+/**
  * Testing unit for rt.cpp
  * Compile example: g++ -o rt_testing rt_testing.cpp -std=c++0x
  */
 
 int main(void)
 {
-routeTable myRouteTable;
-string ASPATH = "1 2 3";
-myRouteTable.addRoute(ASPATH, 0xFF00, 0xFFF0, 0x2344);
-std::cout << "Current routeTable route count: " << myRouteTable.RouteCount() << std::endl;
-return 0;
+	routeTable myRouteTable; // create and initiaize route table
+
+	// set up some routes for testing (these may do not make sense now):
+	string ASPATH = "1 2 4 3"; string ASID ="10000";
+	myRouteTable.addRoute(ASID, ASPATH, 0x000F, 0x8844, 0x2344,1);
+	ASID = "00042"; ASPATH = "1 4 2 3";
+	myRouteTable.addRoute(ASID, ASPATH, 0x000F, 0xFFF0, 0x2344,2);
+	ASID = "20500"; ASPATH = "1";
+	myRouteTable.addRoute(ASID, ASPATH, 0xFFFF, 0x1234, 0xFFFF,0);
+
+	// test the other routines:
+
+	// RouteCount:
+	std::cout << "Current routeTable route count: " << myRouteTable.routeCount() << std::endl;
+
+	// routeDelete:
+
+	return 0;
 }
