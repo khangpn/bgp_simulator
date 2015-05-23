6e351636bd03c85f6160451436d1dff1bc6a126d
juhehe <jussi.heino@utu.fi>
2015-05-23 12:42:32 +0300

    JH: Packet: IP header interface ready, implementation is incomplete.

--- SystemV2.cpp
+++ SystemV2.cpp
@@ -10,6 +10,21 @@
 
 #include "ip_packet.cpp"
 
+struct ip_header_t {
+	unsigned int iph_ihl; // default and minimum header length of five 32b long words
+	unsigned int iph_ver; // 4 for IPv4
+	unsigned int iph_tos; // TOS
+	unsigned short iph_len; // length of packet, 20 bytes is minimum (5*4 bytes is just the header)
+	unsigned int iph_ident;
+	unsigned int iph_flag;
+	unsigned int iph_offset; // Fragmentation Offset (0 for first in fragmented sequence)
+	unsigned int iph_ttl; // Time-To-Live
+	unsigned int iph_protocol; // 6=TCP, but what is good here TODO
+	unsigned int iph_chksum; // calculated, zero for calulation phase
+	unsigned int iph_sourceip;
+	unsigned int iph_destip;
+};
+
 #include <stdio.h>
 #include <stdlib.h> // for malloc
 
@@ -24,6 +39,7 @@ class Packet
 unsigned char *buf = NULL;
 unsigned int packet_size = 0;
 unsigned int header_size = 0;
+unsigned char *message = NULL;
 
 public:
 	Packet() {;};
@@ -31,52 +47,40 @@ public:
 
 	Packet(int IP_PACKET, int TOS, int Identification, int Flags, int FO, int TTL, int Protocol, unsigned int srcIP , unsigned int dstIP)
 	{
-		struct ip_header {
-			unsigned char	iph_ihl;
-			unsigned char	iph_ver;
-			unsigned char		iph_tos;
-			unsigned short int	iph_len;
-			unsigned short int	iph_ident;
-			unsigned char		iph_flag;
-			unsigned short int	iph_offset;
-			unsigned char	iph_ttl;
-			unsigned char	iph_protocol;
-			unsigned short int	iph_chksum;
-			unsigned int	iph_sourceip;
-			unsigned int	iph_destip;
-		};
-		unsigned int iph_ihl = 5; // default and minimum header length of five 32b long words
-		unsigned int iph_ver = 4; // 4 for IPv4
-		unsigned int iph_tos	= 0; // TOS
-		unsigned short iph_len	= 20; // length of packet, 20 bytes is minimum (5*4 bytes is just the header)
-		unsigned int iph_ident	= Identification;
-		unsigned int iph_flag	= Flags;
-		unsigned int iph_offset	= FO; // Fragmentation Offset (0 for first in fragmented sequence)
-		unsigned int iph_ttl	= TTL; // Time-To-Live
-		unsigned int iph_protocol	= 6; // 6=TCP, but what is good here TODO
-		unsigned int iph_chksum	= 0; // calculated, zero for calulation phase
-		unsigned int iph_sourceip	= dstIP;
-		unsigned int iph_destip		= srcIP;
+		ip_header_t iph;// = new ip_header_t; // I do not know C....
 
-		header_size = iph_len;
+		iph.iph_ihl = 5; // default and minimum header length of five 32b long words
+		iph.iph_ver = 4; // 4 for IPv4
+		iph.iph_tos	= 0; // TOS
+		iph.iph_len	= 20; // length of packet, 20 bytes is minimum (5*4 bytes is just the header)
+		iph.iph_ident	= Identification;
+		iph.iph_flag	= Flags;
+		iph.iph_offset	= FO; // Fragmentation Offset (0 for first in fragmented sequence)
+		iph.iph_ttl	= TTL; // Time-To-Live
+		iph.iph_protocol	= 6; // 6=TCP, but what is good here TODO
+		iph.iph_chksum	= 0; // calculated, zero for calulation phase
+		iph.iph_sourceip	= dstIP;
+		iph.iph_destip		= srcIP;
+
+		header_size = iph.iph_len;
 		packet_size = header_size;
 
 		// construct a IP packet bitstream:
 
 		unsigned char byte;
 		int i = 0;
-		buf = (unsigned char*)malloc(iph_len);
+		buf = (unsigned char*)malloc(iph.iph_len);
 		if (buf == NULL) { printf("### ERROR: memory allocation error."); exit(2); }
 
 		// first byte
-		byte = 0xFF & ( iph_ihl + ( iph_ver << 4 ) );
+		byte = 0xFF & ( iph.iph_ihl + ( iph.iph_ver << 4 ) );
 		buf[i++] = byte;
 
-		buf[i++] = iph_tos << 2; // two lowest bits unused/reserved
+		buf[i++] = iph.iph_tos << 2; // two lowest bits unused/reserved
 
-		iph_len = N2H(iph_len);
-		buf[i++] = iph_len >>8; // Length. NB. Endianness is corrected.
-		buf[i++] = iph_len & 0xFF;
+		iph.iph_len = N2H(iph.iph_len);
+		buf[i++] = iph.iph_len >>8; // Length. NB. Endianness is corrected.
+		buf[i++] = iph.iph_len & 0xFF;
 
 		buf[i++] = Identification;
 
@@ -107,7 +111,7 @@ public:
 	 */
 	void Print();
 	unsigned short getChecksum();
-	Packet deserialize();
+	ip_header_t deserialize( char *buf, int size);
 	unsigned char *serialize(); // the checksum will be recalculated
 	unsigned char *getMessage(); // the message payload of Packet
 	void setMessage( unsigned char * message);
@@ -128,6 +132,45 @@ unsigned short Packet::getChecksum()
 {
 	return IPchecksum((unsigned char *)buf, header_size);
 }
+
+/*
+ * Receive incoming buffer to Packet and form IP header stucture
+ * @returns ip_header_t or NULL if incoming packet is not acceptable as IPv4 header
+ */
+ip_header_t Packet::deserialize( char *buf, int size)
+{
+	ip_header_t iph;
+	// TODO
+
+	// step 1 check if IPv4 info is OK
+	// step 2 check IHL
+	// step 3 check length?
+	// step 4 check checksum?
+	// step 5 other? (let me see lecture notes)
+	return iph;
+}
+unsigned char *Packet::serialize() // the checksum will be recalculated
+{
+	// TODO make sure checksum is updated
+	return buf;
+}
+unsigned char *Packet::getMessage()  // the message payload of Packet
+{
+	return this->message;
+}
+
+void Packet::setMessage( unsigned char * message)
+{
+	this->message = message;
+	// TODO update checksum
+}
+
+void Packet::setTTL( int newTTL ) // will set the IP header TTL value
+{
+}
+
+
+
 
 /*
  * Router
